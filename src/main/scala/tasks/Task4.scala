ce69b41f59670e76420e306bee218ea24807d268
alex mazzoni <alexmaz03@hotmail.it>
2026-03-10 10:46:23 +0100

    task 4, evaluate completed

--- src/main/scala/tasks/Task4.scala
+++ src/main/scala/tasks/Task4.scala
@@ -9,6 +9,6 @@ object Expr:
   def evaluate(e: Expr): Int = e match
     case Expr.Literal(x) => x
     case Expr.Add(x: Expr, y: Expr) => evaluate(x) + evaluate(y)
-//    case Expr.Multiply(x) =>
+    case Expr.Multiply(x: Expr, y: Expr) => evaluate(x) * evaluate(y)
 
 //  def show(e: Expr): String = e match
--- src/test/scala/tasks/Task4Test.scala
+++ src/test/scala/tasks/Task4Test.scala
@@ -5,15 +5,21 @@ import org.junit.Assert.*
 import Expr.*
 class Task4Test:
 
+  val x: Expr = Literal(5)
+  val y: Expr = Literal(10)
+  val z: Expr = Literal(2)
+
   @Test
   def testLiteral(): Unit =
-    val constant: Int = 10
-    val literal = Literal(constant)
-    assertEquals(constant, Expr.evaluate(literal))
+    val number: Int = 5
+    assertEquals(number, evaluate(Literal(number)))
 
   @Test
   def testAdd(): Unit =
-    val x: Expr = Literal(10)
-    val y: Expr = Literal(5)
     val add: Expr = Add(x, y)
-    assertEquals(10+5, Expr.evaluate(add))
+    assertEquals(evaluate(x)+evaluate(y), Expr.evaluate(add))
+
+  @Test
+  def testMultiply(): Unit =
+    val multiply: Expr = Multiply(x, y)
+    assertEquals(evaluate(x)*evaluate(y), evaluate(multiply))
